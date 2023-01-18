de822872e16e0c90ee36d6cdf65e0edea597fad1
zoe <emnichtda@gmail.com>
2023-01-18 12:51:29 +0100

    keybinds for move

--- src/main/java/de/emnichtda/fx3dwireframerender/test/TestApplication.java
+++ src/main/java/de/emnichtda/fx3dwireframerender/test/TestApplication.java
@@ -7,18 +7,24 @@ import javafx.application.Platform;
 import javafx.geometry.Point3D;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.chart.BubbleChart;
-import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 
 public class TestApplication extends Application {
 
     Scene mainScene;
+
+    private boolean isClosed = false;
+
+    public double cameraX = 0;
+    public double cameraY = -100;
+    public double cameraZ = 0;
+
+    public double deltaX = 0;
+    public double deltaY = 0;
+    public double deltaZ = 0;
 
     @Override
     public void start(Stage primaryStage) {
@@ -70,7 +76,7 @@ public class TestApplication extends Application {
         frame.addEdge(threeTopThree);
 
 
-        Path path = frame.raycastToPath(new Point3D(0, -50, 0), 100);
+        Path path = frame.raycastToPath(new Point3D(cameraX, cameraY, cameraZ), 500);
 
         p.add(path, 0, 0);
 
@@ -80,32 +86,53 @@ public class TestApplication extends Application {
         primaryStage.setScene(mainScene);
         primaryStage.show();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int i = -50;
-                int focal = 20;
-                while(true){
-                    focal--;
-                    i++;
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    int finalI = i;
-                    int finalFocal = focal;
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            path.getElements().setAll(frame.raycastToPath(new Point3D(0, -100, finalI), 500).getElements());
-                        }
-                    });
+        primaryStage.setOnCloseRequest((windowEvent) -> isClosed = true);
+
+        mainScene.setOnKeyPressed((keyEvent) -> {
+            if (keyEvent.getCode().equals(KeyCode.W)) {
+                deltaY = 1;
+            } else if (keyEvent.getCode().equals(KeyCode.S)) {
+                deltaY = -1;
+            } else if (keyEvent.getCode().equals(KeyCode.A)) {
+                deltaX = -1;
+            } else if (keyEvent.getCode().equals(KeyCode.D)) {
+                deltaX = 1;
+            }
+        });
+
+        mainScene.setOnKeyReleased((keyEvent) -> {
+            if (keyEvent.getCode().equals(KeyCode.W)) {
+                deltaY = 0;
+            } else if (keyEvent.getCode().equals(KeyCode.S)) {
+                deltaY = 0;
+            } else if (keyEvent.getCode().equals(KeyCode.A)) {
+                deltaX = 0;
+            } else if (keyEvent.getCode().equals(KeyCode.D)) {
+                deltaX = 0;
+            }
+        });
+
+        new Thread(() -> {
+            while (!isClosed) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if (deltaX != 0 || deltaY != 0 || deltaZ != 0) {
+                    cameraX += deltaX;
+                    cameraY += deltaY;
+                    cameraZ += deltaZ;
+
+                    Platform.runLater(() -> p.getChildren().set(0, frame.raycastToPath(new Point3D(cameraX, cameraY, cameraZ), 500)));
                 }
             }
         }).start();
 
     }
 
-    public static void main(String[] args){ launch(); }
+    public static void main(String[] args) {
+        launch();
+    }
 }
